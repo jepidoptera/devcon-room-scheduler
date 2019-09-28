870b80a399eb7d414f2bb0fc825f6f82020253ab
jepidoptera <jepidoptera@live.com>
2019-09-28 10:27:58 -0500

    simplified and deleted old stuff

--- routes/router.js
+++ routes/router.js
@@ -15,7 +15,7 @@ process.env.MONGODB_URI ||
 }
 );
 
-router.get('/', (req, res) => {
+router.get('/oldhome', (req, res) => {
     // home page
     console.log('rendering timetable page...');
     db.TimeSlot
@@ -37,95 +37,13 @@ router.get('/', (req, res) => {
         })
         .catch(err => res.status(422).json(err));
 })
-router.get('/signup/:index', (req, res) => {
-    console.log(req);
-    res.render('signup', {index: req.params.index})
-})
-
-router.post('/signup/:index', (req, res) => {
-    console.log(req.body);
-    db.TimeSlot
-    .findOneAndUpdate({ index: req.params.index }, req.body)
-    .then(dbModel => {
-        console.log(JSON.stringify(dbModel));
-        res.render("success", {text: "submitted successfully - redirecting...", redirect: "/"});
-    })
-    .catch(err => res.status(422).json(err));
-})
-
-router.get("/admin", (req, res) => {
-    db.TimeSlot
-        .find({})
-        .sort({ index: 1 })
-        .then(timeslots => {
-            timeslots = timeslots.map(slot => {
-                return {
-                    index: slot.index,
-                    start: timeFormat(slot.start),
-                    owner: slot.owner,
-                    description: slot.description
-                }
-            });
-            // console.log(timeslots);
-            res.render("admin", {
-                timeslots: timeslots
-            })
-        })
-        .catch(err => res.status(422).json(err));
-})
-
-router.get("/admin/login", (req, res) => {
-    // we will make this very easy for now
-    res.render("login", {});
-})
-
-router.post("/admin/login", (req, res) => {
-    // we will make this very easy for now
-    res.render("success", {text: "that password definitely checks out!", redirect: "/admin"});
-})
-
-router.get("/admin/edit/:index", (req, res) => {
-    db.TimeSlot
-        .findOne({index: req.params.index})
-        .then(slot => {
-            console.log("admin editing slot: ", slot);
-            res.render("edit", slot);
-        })
-        .catch(err => res.status(422).json(err))
-})
-
-router.post('/admin/edit/:index', (req, res) => {
-    console.log(req.body);
-    db.TimeSlot
-    .findOneAndUpdate({ index: req.params.index }, req.body)
-    .then(dbModel => {
-        console.log(JSON.stringify(dbModel));
-        res.render("success", {text: "updated successfully - redirecting...", redirect: "/admin"});
-    })
-    .catch(err => res.status(422).json(err));
-})
-
-router.get('/admin/talks', (req, res) => {
-    console.log('retrieving talks...');
-    airTable.getTalks((talks) => {
-        res.render("talks", {talks: talks.map(talk => {
-            let start = new Date(talk.start_at);
-            let end = new Date(talk.end_at);
-            return {
-                ...talk,
-                time:  `${daysOfWeek[start.getDay()]}, 
-                ${start.getHours()}:${start.getMinutes().toString().padEnd(2, "0")} -
-                ${end.getHours()}:${end.getMinutes().toString().padEnd(2, "0")}`        
-            }
-        })});
-    });
-})
 
 router.get('/amphitheater', (req, res) => {
     console.log('retrieving sheduler page...');
     let talks = airTable.getFromRoom("Amphitheater");
     res.render("scheduler", {
         title: "Amphitheater Lightning Talks",
+        rooms: ["amphitheater"],
         bookings: talks.map(talk => {
 
             return {
@@ -143,52 +61,24 @@ router.get('/amphitheater', (req, res) => {
             {name:"description", width: 40},
             {name:"speakers", width: 20}
         ],
+        fields: [
+            {text:"talk name", name: "name", required: true},
+            {text:"description", name: "description"},
+            {text: "speaker(s): (separate names with commas)", name: "speakers", required: true}
+        ],
         time_increment: 5,
         max_consecutive_slots: 2
     });
 })
 
-// router.get('/amphitheater', (req, res) => {
-//     console.log('retrieving talks for amphitheater...');
-//     let talks = airTable.getFromRoom("Amphitheater");
-//     res.render("amphitheater", {talks: talks.map(talk => {
-//         // let start = new Date(talk.start_at);
-//         // let end = new Date(talk.end_at);
-
-//         return {
-//             ...talk,
-//             // converting to UTC so javascript won't fuck with it
-//             // sorry this looks horrible. timezones are annoying
-//             // start_at: `${start.getFullYear()}-${(start.getMonth() + 1).toString().padStart(2,"0")}-${start.getDate().toString().padStart(2,"0")}T${start.getHours().toString().padStart(2,"0")}:${start.getMinutes().toString().padStart(2,"0")}Z`,
-//             // end_at: `${end.getFullYear()}-${(end.getMonth() + 1).toString().padStart(2,"0")}-${end.getDate().toString().padStart(2,"0")}T${end.getHours().toString().padStart(2,"0")}:${end.getMinutes().toString().padStart(2,"0")}Z`,
-//             speakers: talk.speakers.map(speaker => {return {name: speaker}})
-//         }
-//     })});
-// })
-
-router.post('/reserve/amphitheater', (req, res) => {
-    // console.log(JSON.stringify(req.body));
-    airTable.schedule({
-        ...req.body,
-        // turn them into correct data types
-        start_at: new Date(parseInt(req.body.start_at)).toISOString(),
-        end_at: new Date(parseInt(req.body.start_at) + parseInt(req.body.length) * 60000).toISOString(),
-        speakers: req.body.speakers.split(',').map(speaker => speaker.trim().toLowerCase()),
-        room: "Amphitheater"
-    });
-
-    // TODO: send confirmation email (purpose TBD)
-    let email = req.body.email;
-
-    res.render("success", {text: "Thank you for your submission.  You'll receive a confirmation email shortly.", redirect: "/amphitheater"})
-})
-
-router.get("/meetings", (req, res) => {
+router.get("/meeting", (req, res) => {
     let meetings = airTable.getFromRoom("Meeting Room 1")
         .concat(airTable.getFromRoom("Meeting Room 2"));
-        console.log(meetings);
+        // console.log(meetings);
         res.render("scheduler", {
             title: "Meeting Rooms",
+            rooms: ["meeting room 1", "meeting room 2"],
+            // obscure the actual names of the groups
             bookings: meetings.map(meeting => {return {...meeting, name: "occupied"}}),
             first_day: "10-08",
             last_day: "10-11",
@@ -198,11 +88,36 @@ router.get("/meetings", (req, res) => {
                 {name:"status", width: 50},
                 {name:"time", width: 50},
             ],
+            fields: [
+                {text:"group name:", name: "name", required: true}
+            ],
             time_increment: 30,
             max_consecutive_slots: 2
         });
     })
 
+    router.post('/reserve', (req, res) => {
+        // console.log(JSON.stringify(req.body));
+        airTable.schedule({
+            ...req.body,
+            // turn them into correct data types
+            start_at: new Date(parseInt(req.body.start_at)).toISOString(),
+            end_at: new Date(parseInt(req.body.start_at) + parseInt(req.body.length) * 60000).toISOString(),
+            speakers: req.body.speakers 
+                ? req.body.speakers.split(',').map(speaker => speaker.trim().toLowerCase())
+                : undefined,
+        });
+    
+        // TODO: send confirmation email (purpose TBD)
+        let email = req.body.email;
+    
+        res.render("success", {
+            text: "Thank you for your submission.  You'll receive a confirmation email shortly.", 
+            redirect: `/${req.body.room.split(' ')[0]}`
+        })
+    })
+    
+    
 // router.get("/reset", (req, res) => {
 //     // reset database
 //     require("../scripts/seedDB");
